443719154c13985df53b79d090062e8426b0eea0
minmugil <alsanrlf@naver.com>
2025-01-23 23:41:24 +0900

    feat: BlogNav개발
    Side Navigation 개발

--- src/page/blog/feature/BlogNav.tsx
+++ src/page/blog/feature/BlogNav.tsx
@@ -1,18 +1,67 @@
+import Image from 'next/image';
 import React from 'react';
+import GitHubIcon from 'public/icons/github-mark.svg';
+import ResumeIcon from 'public/icons/file.svg';
 
 const BlogNav = () => {
-    return (
-        <nav className="bg-gray-800 p-4" style={{position: 'fixed', top: 0, left: 0, right: 0}}>
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="text-white text-lg font-semibold">My Blog</div>
-                <div className="space-x-4">
-                    <a href="#" className="text-gray-300 hover:text-white">Home</a>
-                    <a href="#" className="text-gray-300 hover:text-white">About</a>
-                    <a href="#" className="text-gray-300 hover:text-white">Contact</a>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <aside className="bg-white p-8 rounded-lg flex flex-col gap-4">
+      <Welcome />
+      {/* <DarkModeToggle /> */}
+      {/* <PostFilter /> */}
+      <div style={{ display: 'flex', gap: 16 }}>
+        <ExternalLink title="Resume" href={'/'} icon={ResumeIcon} />
+        <ExternalLink
+          title="GitHub"
+          href={'https://github.com/gilmujjang'}
+          icon={GitHubIcon}
+        />
+      </div>
+      <Copyright />
+    </aside>
+  );
+};
+
+const Welcome = () => {
+  return <span className="text-2xl">민무길의 블로그</span>;
+};
+
+const DarkModeToggle = () => {
+  return <div>Dark Mode Toggle</div>;
+};
+
+const PostFilter = () => {
+  return (
+    <>
+      <span className="text-xs">Categories</span>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+        <span className="text-sm">All</span>
+        <span className="text-sm">Development</span>
+        <span className="text-sm">Investment</span>
+      </div>
+    </>
+  );
+};
+
+const ExternalLink = ({
+  title,
+  href,
+  icon,
+}: {
+  title: string;
+  href: string;
+  icon: any;
+}) => {
+  return (
+    <a className="text-base flex gap-1" href={href}>
+      <Image src={icon} alt="GitHub Icon" width={24} height={24} />
+      <span>{title}</span>
+    </a>
+  );
+};
+
+const Copyright = () => {
+  return <span className="text-sm">Copyright 민무길</span>;
 };
 
 export default BlogNav;
