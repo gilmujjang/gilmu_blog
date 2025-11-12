6e24cf0e32b24dc7dde4a9032c50be0a9eb696ca
gilmujjang <alsanrlf@naver.com>
2025-11-12 14:36:01 +0900

    fix: 파일명 대소문자 변경

---
+++ src/feature/Header.tsx
@@ -0,0 +1,78 @@
+import DarkModeToggle from '@/components/DarkModeToggle';
+import ScrollIndicator from '@/components/ScrollIndicator';
+import FileIcon from '@/components/icons/FileIcon';
+import GithubIcon from '@/components/icons/GithubIcon';
+import { headers } from 'next/headers';
+
+export default function Header() {
+  const __DEV__ = process.env.NODE_ENV === 'development';
+  const headersList = headers();
+  const host = headersList.get('host') || '';
+  const subdomain = host.split('.')[0];
+  return (
+    <header className="w-full sticky top-0 border-b border-gray-200 dark:border-gray-700 transition-colors duration-1000">
+      <div className="h-[64px] flex items-center justify-between font-medium px-4 md:static md:h-20 bg-background/95 p-6 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <span>
+          <a
+            className="text-BLACK dark:text-white rounded-full px-4 py-1 text-center text-sm hover:text-primary bg-muted font-medium text-primary"
+            href={
+              __DEV__ ? 'http://blog.localhost:3000' : 'https://blog.mugil.dev'
+            }
+          >
+            민무길 블로그
+          </a>
+        </span>
+        <span className="flex items-center gap-4">
+          {subdomain === 'blog' ? (
+            <ExternalLink
+              title="Resume"
+              href={
+                __DEV__
+                  ? 'http://resume.localhost:3000'
+                  : 'https://resume.mugil.dev'
+              }
+              icon={FileIcon}
+            />
+          ) : (
+            <ExternalLink
+              title="Blog"
+              href={
+                __DEV__
+                  ? 'http://blog.localhost:3000'
+                  : 'https://blog.mugil.dev'
+              }
+              icon={FileIcon}
+            />
+          )}
+          <ExternalLink
+            title="GitHub"
+            href={'https://github.com/gilmujjang'}
+            icon={GithubIcon}
+          />
+          <DarkModeToggle />
+        </span>
+      </div>
+      <ScrollIndicator />
+    </header>
+  );
+}
+
+const ExternalLink = ({
+  title,
+  href,
+  icon: Icon,
+}: {
+  title: string;
+  href: string;
+  icon: any;
+}) => {
+  return (
+    <a
+      className="text-base flex gap-1 text-gray-900 dark:text-gray-100"
+      href={href}
+    >
+      <Icon />
+      <span>{title}</span>
+    </a>
+  );
+};
