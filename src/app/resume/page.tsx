4e19a195acbf40a150df5fff4e5576a12091b08b
gilmujjang <alsanrlf@naver.com>
2025-11-17 21:47:47 +0900

    post: ReactNative 빌드 포스트수정

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -8,7 +8,7 @@ export default function Resume() {
     <section className="max-w-4xl mx-auto p-8 flex flex-col gap-24 md:gap-32">
       <Introduce />
       <WorkExperience />
-      <Project />
+      {/* <Project /> */}
       <Education />
     </section>
   );
