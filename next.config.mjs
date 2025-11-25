992c4eabb203cb8f7c0294b0af5d99f4ae4315fb
gilmujjang <alsanrlf@naver.com>
2025-11-25 13:32:17 +0900

    post: WebView 렌더링 성능분석 포스트 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import createMDX from '@next/mdx';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['mdx', 'ts', 'tsx'],
+  images: {
+    domains: ['velog.velcdn.com'],
+  },
 };
 
 const widthMDX = createMDX({});
